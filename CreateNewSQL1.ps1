1ed91ab7dfb499641f5bba448219a3c5d1223baf
Kevin <RhinoDBA@hotmail.com>
2022-05-27 11:34:24 -0400

    Friday morning Checkin: PowerShell deployment

---
+++ CreateNewSQL1.ps1
@@ -0,0 +1,74 @@
+# Make Initial Connection.  Azure Commercial by defultt
+# If connecting to Azure Gov, it must be specified. 
+Connect-AzAccount -EnvironmentName AzureUSGovernment 
+
+#Get a list of your subscriptions.  If you have more than one, the particular target subscription needs to be set.
+#Get-AzSubscription
+
+# If you forget to set the scope (or context) of the Azure PowerSell Az commands to the correct Azure Subscription, then you may end up provisioning or deleting resources in the wrong Azure Subscription
+Set-AzContext -SubscriptionName "US Gov FedCon"
+
+# Get a list of Azure Locations so you know which location to add to your globar variable
+Get-AzLocation
+
+## Global variables
+$Location = "usgovvirginia"
+$ResourceGroupName = "rgazsqlvm"
+
+## Storage
+$StorageName = $ResourceGroupName + "storage"
+$StorageSku = "Premium_LRS"
+
+## Network Properties 
+$InterfaceName = $ResourceGroupName + "ServerInterface"
+$NsgName = $ResourceGroupName + "nsg"
+$VNetName = $ResourceGroupName + "VNet"
+$SubnetName = "Default"
+$VNetAddressPrefix = "10.0.0.0/16"
+$VNetSubnetAddressPrefix = "10.0.0.0/24"
+$TCPIPAllocationMethod = "Dynamic"
+$DomainName = $ResourceGroupName
+
+##Compute
+$VMName = $ResourceGroupName + "VM"
+$ComputerName = $ResourceGroupName + "Server"
+$VMSize = "Standard_DS1_V2"
+$OSDiskName = $VMName + "OSDisk"
+
+##Image
+$PublisherName = "MicrosoftSQLServer"
+$OfferName = "SQL2017-WS2016"
+$Sku = "SQLDEV"
+$Version = "latest"
+
+# Resource Group
+New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+
+# Storage
+$StorageAccount = New-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageName -SkuName $StorageSku -Kind "Storage" -Location $Location
+
+# Network
+$SubnetConfig = New-AzVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix $VNetSubnetAddressPrefix
+$VNet = New-AzVirtualNetwork -Name $VNetName -ResourceGroupName $ResourceGroupName -Location $Location -AddressPrefix $VNetAddressPrefix -Subnet $SubnetConfig
+$PublicIp = New-AzPublicIpAddress -Name $InterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod $TCPIPAllocationMethod -DomainNameLabel $DomainName
+$NsgRuleRDP = New-AzNetworkSecurityRuleConfig -Name "RDPRule" -Protocol Tcp -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389 -Access Allow
+$NsgRuleSQL = New-AzNetworkSecurityRuleConfig -Name "MSSQLRule"  -Protocol Tcp -Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 1433 -Access Allow
+$Nsg = New-AzNetworkSecurityGroup -ResourceGroupName $ResourceGroupName -Location $Location -Name $NsgName -SecurityRules $NsgRuleRDP,$NsgRuleSQL
+$Interface = New-AzNetworkInterface -Name $InterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $VNet.Subnets[0].Id -PublicIpAddressId $PublicIp.Id -NetworkSecurityGroupId $Nsg.Id
+
+# Compute
+$VirtualMachine = New-AzVMConfig -VMName $VMName -VMSize $VMSize
+$Credential = Get-Credential -Message "Type the name and password of the local administrator account."
+$VirtualMachine = Set-AzVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $ComputerName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate #-TimeZone = $TimeZone
+$VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine -Id $Interface.Id
+$OSDiskUri = $StorageAccount.PrimaryEndpoints.Blob.ToString() + "vhds/" + $OSDiskName + ".vhd"
+$VirtualMachine = Set-AzVMOSDisk -VM $VirtualMachine -Name $OSDiskName -VhdUri $OSDiskUri -Caching ReadOnly -CreateOption FromImage
+
+# Image
+$VirtualMachine = Set-AzVMSourceImage -VM $VirtualMachine -PublisherName $PublisherName -Offer $OfferName -Skus $Sku -Version $Version
+
+# Create the VM in Azure
+New-AzVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $VirtualMachine
+
+# Add the SQL IaaS Extension, and choose the license type
+New-AzSqlVM -ResourceGroupName $ResourceGroupName -Name $VMName -Location $Location -LicenseType PAYG
